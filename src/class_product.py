5ccda9ae21652c5f06da317fde9b51cecc310a45
Your Name <jeiromkrizhanovskiyd@gmail.com>
2024-10-13 19:29:09 +0500

    Work is class 'product'

--- src/class_product.py
+++ src/class_product.py
@@ -10,5 +10,34 @@ class Product:
         """Передаются одноименные переменные, для дальнейшего использования как отдельно, так и в Category"""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+
+    @property
+    def price(self):
+        return self.__price
+
+
+
+
+    @price.setter
+    def price(self, value: int):
+        if value <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = value
+
+
+    @classmethod
+    def new_product(cls, product_data):
+        name = product_data.get("name")
+        description = product_data.get("description")
+        price = product_data.get("price")
+        quantity = product_data.get("quantity")
+        return cls(name, description, price, quantity)
+
+    def __repr__(self):
+        return f"Product(name='{self.name}', description='{self.description}', price={self.price}, quantity={self.quantity})"
+
+
