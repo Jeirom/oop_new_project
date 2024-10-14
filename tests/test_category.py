b577c0fd0864358631ff1aa56d4836cf2fe0fb27
Your Name <jeiromkrizhanovskiyd@gmail.com>
2024-10-14 13:43:18 +0500

    Rename tests\

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,6 +1,17 @@
+from src.category import Category
+
+
+
 def test_class_category(second_category,firs_category):
     assert second_category.name == "Техника"
     assert second_category.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
     assert second_category.category_count == 2
     assert second_category.product_count == 4
     assert len(firs_category.products) == 1
+
+def test_add_category(firs_category):
+    assert Category.product_count == 1
+
+def test_product_list(firs_category):
+    # assert Category.product_list ==
+    pass # дописать как пофиксишь ошибку
