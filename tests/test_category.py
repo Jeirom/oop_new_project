d7e8942bd8eefbfe21b4e145b78a439078ef17ef
Your Name <jeiromkrizhanovskiyd@gmail.com>
2024-10-14 17:51:31 +0500

    add test in test_category.py

--- tests/test_category.py
+++ tests/test_category.py
@@ -12,6 +12,19 @@ def test_class_category(second_category,firs_category):
 def test_add_category(firs_category):
     assert Category.product_count == 1
 
-def test_product_list(firs_category):
-    # assert Category.product_list ==
-    pass # дописать как пофиксишь ошибку
+def test_category_tv(category_tv, product_4):
+    assert category_tv.name == "Телевизоры"
+    assert category_tv.description == "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником"
+    assert category_tv.products == [product_4]
+
+
+def test_category_smart(category_smart, product_1, product_2, product_3):
+    assert category_smart.name == "Смартфоны"
+    assert category_smart.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
+    assert category_smart.products == [product_1, product_2, product_3]
+
+
+def test_category(category_smart, product_4):
+    category_smart.add_product(product_4)
+    category_smart.add_product(new_product)
+    assert Category.product_count == 9
